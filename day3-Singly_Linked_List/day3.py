76f5fa86543c301a93f537d42aa8a3fc3f1a008c
dilliram-code <chaudharydr23@gmail.com>
2025-10-13 23:01:41 +0545

    create a class Node and with para: data and next_node along with the objects

---
+++ day3-Singly_Linked_List/day3.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init(self, data=None, next_node=None):
+        self.data = data
+        self.next = next_node
+    
+
+# initialize the nodes (creating objects of class Node)
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+node5 = Node(5)
+node6 = Node(6)
