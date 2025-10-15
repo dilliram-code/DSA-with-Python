b770020c92ae998704bd6b2a07fb8daa0187d708
dilliram-code <chaudharydr23@gmail.com>
2025-10-15 22:50:22 +0545

    create the nodes for the SSL

---
+++ day3-Singly_Linked_List/insertion_at_the_beginning.py
@@ -0,0 +1,14 @@
+# insert a node at the beginning of the list
+
+class Node:
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next = next_node
+
+# create objects
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+node4 = Node(40)
+node5 = Node(50)
+
