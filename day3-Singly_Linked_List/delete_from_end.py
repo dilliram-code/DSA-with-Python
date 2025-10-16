c2bb7fcb9393dbb7f2fcb8aa08674498e670dc0e
dilliram-code <chaudharydr23@gmail.com>
2025-10-16 21:05:13 +0545

    delete the last node from the SSL

---
+++ day3-Singly_Linked_List/delete_from_end.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next = next_node
+
+# create nodes
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+node4 = Node(40)
+
+# link nodes
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+# delete the last node
+head = node1
+current = head
+
+while current.next.next is not None:
+    current = current.next
+current.next = None
