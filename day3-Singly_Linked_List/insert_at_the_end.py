1b6320613a387ca217c7ae7259f7f3dd055a75c6
dilliram-code <chaudharydr23@gmail.com>
2025-10-16 18:26:26 +0545

    create the nodes for the SSL

---
+++ day3-Singly_Linked_List/insert_at_the_end.py
@@ -0,0 +1,14 @@
+# insert an element the end of the linked list
+
+class Node:
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next = next_node
+
+# create nodes (create the objects of the class Node)
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+node4 = Node(40)
+node5 = Node(50)
+
