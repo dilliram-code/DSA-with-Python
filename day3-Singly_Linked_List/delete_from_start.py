c272147afbdeef9643ad5dcd391d25ca043d1446
dilliram-code <chaudharydr23@gmail.com>
2025-10-16 20:42:39 +0545

    create nodes

---
+++ day3-Singly_Linked_List/delete_from_start.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next = next_node
+
+# create nodes
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+node4 = Node(40)
+
